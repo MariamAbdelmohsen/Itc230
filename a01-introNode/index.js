1f652f4aece946ce43f7f2fc55e51ef88bfe1f72
Mariam <MariamAbdelmohsen@users.noreply.github.com>
2018-04-05 21:03:34 -0700

    bug

--- a01-introNode/index.js
+++ a01-introNode/index.js
@@ -1,19 +1,37 @@
-var http = require("http");
+var http = require("http"),
+    fs = require("fs");
+
+
+function serveStatic(res, path, contentType, responseCode) {
+    if (!responseCode) responseCode = 200;
+    console.log(__dirname + path)
+    fs.readFile(__dirname + path, function(err, data) {
+        // if (err) {
+        //     res.writeHead(500, { 'Content-Type': 'text/plain' });
+        //     res.end('Internal Server Error');
+        // } else {
+        //     res.writeHead(responseCode, { 'Content-Type': contentType });
+        //     res.end(data);
+        // }
+    });
+}
+
 http.createServer(function(req, res) {
     var path = req.url.toLowerCase();
     switch (path) {
         case '/':
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end('Home Page!');
+            // app.get('/../a01-introNode/public/home.html', function(req, res) {
+            //     res.send('/../a01-introNode/public/home.html')
+            // });
+            serveStatic(res, '/../../public/home.html', 'text/html');
             break;
         case '/about':
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end('About Page!');
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('This is the About Page!');
             break;
         default:
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.end('Not found');
-            break;
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('404: Page -- Not found');
     }
 
 }).listen(process.env.PORT || 3000);
